2af7882016dd616610fb74f3f46f485eab209960
shubhamwhj <shubham.verma@whitehatjr.com>
2021-01-17 14:20:04 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -15,9 +15,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
-
+    ground = new Ground(600,height,1200,20);
+    platform =new Ground(150,300,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -57,6 +56,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display()
     bird.display();
 }
